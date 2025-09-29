eadca633acf4308b106ae1ba90a5ea2743a832bc
João Pedro Rodrigues Mattos <joao_pedro_mattos@hotmail.com>
2025-09-28 21:41:40 -0500

    Time: 3 ms (29.45%), Space: 18 MB (34.6%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str):
+        if s == '':
+            return True
+        
+        if not s:
+            return False
+        
+        stack = []
+        possibilities = {
+            '[' : ']',
+            '(' : ')',
+            '{' : '}'
+        }
+        
+        for cur_char in s:
+            if cur_char in set(possibilities.values()):
+                if stack == []:
+                    return False
+                
+                popped_char = stack.pop()
+                if possibilities[popped_char] != cur_char:
+                    return False
+            else:
+                stack.append(cur_char)
+                
+        if stack != []:
+            return False
+            
+        return True
+            
