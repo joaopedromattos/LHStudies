3c0d90b97dc275c37bb93e0e5122f0f77a77e55a
João Pedro Rodrigues Mattos <joao_pedro_mattos@hotmail.com>
2025-10-07 21:58:26 -0500

    Time: 9 ms (92.93%), Space: 18.3 MB (79.51%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
@@ -23,71 +23,72 @@ Actions :
 
 popSmallest() -> Time => O(1) Space => O(1)
 
-addBack() -> Time => O(1) Space => O(1)
+addBack() -> Time => O(N), being N the number of previous addbacks 
 
 '''
 import bisect
 from collections import deque
 
 
-# class SmallestInfiniteSet:
-
-#     def __init__(self):
-#         self.minimum_in_set = 1
-#         self.cur_minimum = self.minimum_in_set
-#         self.addBackHistory = []
-        
-
-#     def popSmallest(self) -> int:
-
-        
-#         minimum = self.cur_minimum
-
-#         if self.addBackHistory:
-#             self.addBackHistory.remove(self.cur_minimum)
-#             self.cur_minimum = min(self.addBackHistory) if self.addBackHistory else self.minimum_in_set
-#         else:
-#             self.minimum_in_set += 1
-#             self.cur_minimum = self.minimum_in_set
-
-#         return minimum
-        
-
-#     def addBack(self, num: int) -> None:
-
-#         if num < self.minimum_in_set:
-#             # out of order add back
-#             self.addBackHistory.append(num)
-#             self.cur_minimum = min(self.addBackHistory)
-
-
-
 class SmallestInfiniteSet:
 
     def __init__(self):
-        self.inf_set = [inf]
-        self.cur_minimum = 1
+        self.minimum_in_set = 1
+        self.cur_minimum = self.minimum_in_set
+        self.addBackHistory = []
         
 
     def popSmallest(self) -> int:
 
-        if self.inf_set[0] == inf:
-            minimum = self.cur_minimum
-            self.cur_minimum += 1
+        
+        minimum = self.cur_minimum
+
+        if self.addBackHistory:
+            self.addBackHistory.remove(self.cur_minimum)
+            self.cur_minimum = min(self.addBackHistory) if self.addBackHistory else self.minimum_in_set
         else:
-            minimum = self.inf_set[0]
-            self.inf_set = self.inf_set[1:] # min element
+            self.minimum_in_set += 1
+            self.cur_minimum = self.minimum_in_set
 
         return minimum
         
 
     def addBack(self, num: int) -> None:
-        if num < self.cur_minimum:
-            i = bisect.bisect_left(self.inf_set, num)
-            if self.inf_set[i] != num:
-                self.inf_set.insert(i, num)
 
-            print(self.inf_set)
+        if num < self.minimum_in_set:
+            # out of order add back
+            if not num in self.addBackHistory:
+                self.addBackHistory.append(num)
+                self.cur_minimum = min(self.addBackHistory)
+
+
+
+# class SmallestInfiniteSet:
+
+#     def __init__(self):
+#         self.inf_set = [inf]
+#         self.cur_minimum = 1
+        
+
+#     def popSmallest(self) -> int:
+
+#         if self.inf_set[0] == inf:
+#             minimum = self.cur_minimum
+#             self.cur_minimum += 1
+#         else:
+#             minimum = self.inf_set[0]
+#             self.inf_set = self.inf_set[1:] # min element
+
+#         return minimum
+        
+
+#     def addBack(self, num: int) -> None:
+#         if num < self.cur_minimum:
+#             i = bisect.bisect_left(self.inf_set, num)
+#             if self.inf_set[i] != num:
+#                 self.inf_set.insert(i, num)
+
+#             print(self.inf_set)
 
 
 
