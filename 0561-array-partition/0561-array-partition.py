b443471ff2b0404f778f6ed5e117f14a1d3bb770
João Pedro Rodrigues Mattos <joao_pedro_mattos@hotmail.com>
2025-10-16 12:25:18 -0500

    Time: 39 ms (12.93%), Space: 19.8 MB (12.41%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,14 +1,32 @@
 '''
-[1,4,3,2] -> sort() -> [1, 2, 3, 4] -> min(1, 2) + min(3, 4) => 4
+Input-> List[int] -> 2 * n
+output -> int -> sum(list[(int, int)])
 
-Time O(nlogn) -> O(1) / O(n)
+
+Tests: 
+[1000, 1001, 1, 2] -> min(1, 2) + min(1000, 1001) -> 1001
+- Main principle : The maximum sum is the one that minimizes the differences between each pair summed
+    - Large numbers paired with large numbers
+    - small numbers paired with small numbers
+
+[inf, 2, -inf, 1] -> min(1, -inf), min(inf, 2) -> 3
+
+
+Algo: 
+Sort array -> sum pairs
+
+
+
+
 '''
+
+
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        
-        sorted_nums = list(sorted(nums))
-        min_sum = 0
-        for i in range(0, len(sorted_nums), 2):
-            min_sum += min(sorted_nums[i], sorted_nums[i + 1])
+        new_nums = sorted(nums)
+        s = 0
+        for i in range(0, len(nums) - 1, 2):
+            s += min(new_nums[i], new_nums[i + 1])
 
-        return min_sum
+        return s
+        
