2f78a733454762e052808738ba86c0e050a898ba
João Pedro Rodrigues Mattos <joao_pedro_mattos@hotmail.com>
2025-10-16 17:30:06 -0500

    Time: 99 ms (7.75%), Space: 19 MB (21.04%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -0,0 +1,41 @@
+'''
+In: num -> str
+
+Out: odd_num -> str
+
+Test:
+222222221 -> 222222221
+
+......X______ 
+
+
+221222222 -> 221
+
+Explanation:
+- If X the leftmost odd number, everything that comes before X is part of the biggest odd number.
+- If _____ is composed only of even numbers, then .......X is the largest odd number.
+- There will never be something like ___.....X______, since every prefix to the odd number will be part of the odd number.
+- We can scan the string looking for the index of the rightmost odd number.
+
+
+
+Time O(N)
+Space(1) 
+'''
+
+
+
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        last_odd_number_idx = -1
+        for i in range(len(num)):
+            if int(num[i]) % 2 != 0:
+                last_odd_number_idx = i
+        
+        if last_odd_number_idx == -1:
+            return ""
+
+        return num[:last_odd_number_idx + 1]
+
+
+        
