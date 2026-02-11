5f3e171ffb708d7f454f92ce95aea0f660d8eaa3
João Pedro Rodrigues Mattos <joao_pedro_mattos@hotmail.com>
2026-02-10 21:22:28 -0300

    Time: 15 ms (6.47%), Space: 21.5 MB (8.91%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -24,18 +24,77 @@ complete_courses = {3, 4, 2, 5, 1}
 
 Time: O(|V| + |V| * |E|), Space : O(|V|) -> Not considering inputs and outputs
 '''
-import heapq
+# import heapq
+# class Solution:
+#     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+
+#         G = {i : {'locks' : set(), 'locked_by': set()} for i in range(numCourses)}
+#         for u, v in prerequisites:
+#             G[u]['locked_by'].add(v)
+#             G[v]['locks'].add(u)
+
+
+#         to_complete = [(0, course) for course in G.keys() if G[course]['locked_by'] == set()]
+#         heapq.heapify(to_complete)
+
+#         print("G", G)
+#         print("To_complete", to_complete)
+#         if not to_complete:
+#             # No starting points...
+#             print("First")
+#             return []
+
+#         solution = []
+#         completed_courses = set()
+
+#         while to_complete:
+#             pre_reqs, course = heapq.heappop(to_complete)
+
+#             # Check if current course can be completed (all pre-reqs have been completed):
+#             if G[course]['locked_by'] <= completed_courses and (course not in completed_courses):
+#                 solution.append(course)
+#                 completed_courses.add(course)
+
+#                 # If a course is completed, we can add its dependencies:
+#                 for locked_course in G[course]['locks']:
+#                     heapq.heappush(to_complete, (len(G[locked_course]['locked_by']), locked_course))
+    
+#                 if len(solution) == numCourses:
+#                     return solution
+
+
+#         print("Last\t", solution, numCourses)
+#         return []
+
+
+
+'''
+
+Solution #2
+
+We just need to keep track of a global variable that counts the in-degree of each node. 
+
+Then we run bfs
+
+
+
+'''
+
+
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
 
         G = {i : {'locks' : set(), 'locked_by': set()} for i in range(numCourses)}
+        indegree = [0] * numCourses
         for u, v in prerequisites:
             G[u]['locked_by'].add(v)
             G[v]['locks'].add(u)
+            indegree[u] += 1
 
 
-        to_complete = [(0, course) for course in G.keys() if G[course]['locked_by'] == set()]
-        heapq.heapify(to_complete)
+
+        to_complete = deque([course for course in G.keys() if G[course]['locked_by'] == set()])
 
         print("G", G)
         print("To_complete", to_complete)
@@ -48,25 +107,22 @@ class Solution:
         completed_courses = set()
 
         while to_complete:
-            pre_reqs, course = heapq.heappop(to_complete)
+            course = to_complete.popleft()
 
-            # Check if current course can be completed (all pre-reqs have been completed):
-            if G[course]['locked_by'] <= completed_courses and (course not in completed_courses):
-                solution.append(course)
-                completed_courses.add(course)
+            solution.append(course)
 
-                # If a course is completed, we can add its dependencies:
-                for locked_course in G[course]['locks']:
-                    heapq.heappush(to_complete, (len(G[locked_course]['locked_by']), locked_course))
-    
-                if len(solution) == numCourses:
-                    return solution
+            for neigh_course in G[course]['locks']:
+                indegree[neigh_course] -= 1
+
+                if indegree[neigh_course] == 0:
+                    to_complete.append(neigh_course)
 
 
-        print("Last\t", solution, numCourses)
-        return []
 
-
+        if len(solution) == numCourses:
+            return solution
+        else:
+            return []
 
 
             
