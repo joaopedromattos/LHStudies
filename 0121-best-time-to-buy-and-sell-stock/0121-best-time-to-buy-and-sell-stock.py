746f9fad496bbf9d5b23ca630d92ceff6cfdd495
João Pedro Rodrigues Mattos <joao_pedro_mattos@hotmail.com>
2026-03-10 10:31:52 -0500

    Time: 45 ms (63.05%), Space: 28.5 MB (98.96%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,27 +1,57 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        # prices = [7,1,5,3,6,4]
+'''
+In: List[Int], Out: Int
 
-        high, low, profit = prices[0], prices[0], 0
-        if not prices:
-            return 0
+[1 2 3 4 5 6]
+[1 2 -1000 4 5 6]
 
-        # Logic
-        for cur_price in prices:
-            if cur_price < low:
-                low = cur_price
-                high = low
+[1 2 -1000 4 5 -1000]
+
+Principle: Find the first day, after the "minimum" = 0, then stop at the last day before the "minimum" = 0.
+    - Pre-fix sum
+
+
+[1 2 0 4 5 0]
+ 0 1  3. 3 4   5
+
+
+
+'''
+
+
+# class Solution: 
+#     def maxProfit(self, prices: List[int]) -> int:
+
+#         pfs = [0] * (len(prices) + 1)
+#         for i in range(1, len(prices)):
+#             pfs[i] = pfs[i-1] + prices[i-1]
+
+#         print(pfs)
+
+#         start = 0
+#         max_profit = -1
+#         for i in range(1, len(pfs)):
+#             if pfs[i] < pfs[start]:
+#                 start = i
             
-            if cur_price > high and cur_price - low > profit:
+#             if pfs[i] - pfs[start] >  max_profit:
+#                 end = i
                 
-                high = cur_price
-                profit = high - low
 
-        profit = max(profit, 0)
-        
-        return profit
-
-
-
+#         return prices[end - 1] - prices[start]
 
         
+# [1 2 0 4 5 0]
+#  1 1 1 0 0 0
+
+class Solution: 
+    def maxProfit(self, prices: List[int]) -> int:
+
+        min_so_far = inf
+        max_profit = 0
+        for price in prices:
+            if price < min_so_far:
+                min_so_far = price
+
+            max_profit = max(price - min_so_far, max_profit)
+
+        return max_profit
