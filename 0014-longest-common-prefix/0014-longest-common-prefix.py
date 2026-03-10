b94cb0b8801cccbffcdade3141207c85280b4110
João Pedro Rodrigues Mattos <joao_pedro_mattos@hotmail.com>
2026-03-10 12:30:27 -0500

    Time: 3 ms (22.4%), Space: 19.6 MB (6.1%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -48,7 +48,6 @@ class Solution:
             if not i in aux:
                 aux[i] = {}
             aux = aux[i]
-        aux['_'] = True
 
         max_prefix = len(strs[0])
         for word in strs[1:]:
@@ -57,6 +56,59 @@ class Solution:
                 return ""
 
         return strs[0][:max_prefix]
+
+
+'''
+In: List[Str], Out: Str
+
+
+Principle: Intersection over strings. For every given string i, we can check for "how long" it matches *any* other string in a set.
+    - By induction, the intersection of one against all, is the intersection of all
+
+
+'abcd', 'ab', 'bc', "abc"
+
+abcd => abc, bc
+
+potential_len = 4
+
+abcd, abc, potential_len=3
+
+abcd, ab = potential_len=2
+
+abcd, bc = potential_len = 0
+
+return ""
+
+
+
+'''
+
+
+
+
+
+class Solution:
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        blueprint = strs[0]
+
+        potential_len = len(blueprint)
+
+        for cur_str in strs[1:]:
+            cur_potential_len = 0
+            while cur_potential_len < len(cur_str) and cur_potential_len < len(blueprint):
+                # Counts until when prefix(blue_print) matches prefix(cur_str)
+                if cur_str[cur_potential_len] == blueprint[cur_potential_len]:
+                    cur_potential_len += 1
+                else:
+                    break
+
+            potential_len = min(potential_len, cur_potential_len)
+
+        return strs[0][:potential_len]
+
             
 
         
